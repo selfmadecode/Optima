f93eb3dcaf000a54c6a709edeca0a8de9399c578
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2022-05-11 15:36:40 +0100

    ADDED BASE CONTROLLER

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Optima.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Optima.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected IActionResult ReturnResponse(dynamic model)
+        {
+            if (model.Status == RequestExecution.Successful)
+            {
+                return Ok(model);
+            }
+
+            return BadRequest(model);
+        }
+    }
+    
+}
