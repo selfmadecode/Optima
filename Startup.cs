391f8a18c0972027b71f0f3829895ec3bf2cb419
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2022-05-11 14:48:35 +0100

    ADDED DBCONTEXT AND SWAGGER UI

--- Startup.cs
+++ Startup.cs
@@ -4,10 +4,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Optima.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +30,9 @@ namespace Optima
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureEntityFrameworkDbContext(services);
+            ConfigureSwagger(services);
+
             services.AddControllers();
         }
 
@@ -45,10 +51,40 @@ namespace Optima
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Optima API v1");
+                c.DisplayRequestDuration();
+                c.DefaultModelsExpandDepth(-1);
+            });
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
         }
+
+        public void ConfigureEntityFrameworkDbContext(IServiceCollection services)
+        {
+            string dbConnectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    dbConnectionString,
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        }
+
+        public void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.DescribeAllParametersInCamelCase();
+                c.SwaggerDoc("v1", new OpenApiInfo { Version = "v1", Title = "Optima API" });
+            });
+        }
+
+
     }
+    
 }
