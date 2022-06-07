d61281d280f4728f3983c2711c656bfb918cc82c
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2022-06-07 17:31:41 +0100

    Added SaleCard DTO

---
+++ Models/DTO/CardSaleDTO/SellCardDTO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Optima.Models.DTO.CardSaleDTO
+{
+    public class SellCardDTO
+    {
+        public List<CardTypeDTO> CardTypeDTO { get; set; }
+        public List<IFormFile> CardImages { get; set; }
+    }
+
+    public class CardTypeDTO
+    {
+        [Required]
+        public Guid CardTypeDenominationId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
