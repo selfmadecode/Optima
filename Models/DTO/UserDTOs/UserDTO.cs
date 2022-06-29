d85fbfdf2ab2c723f1fc65ea1f91505a80756d35
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2022-06-29 12:58:21 +0100

    Added AdminDTO

--- Models/DTO/UserDTOs/UserDTO.cs
+++ Models/DTO/UserDTOs/UserDTO.cs
@@ -37,4 +37,12 @@ namespace Optima.Models.DTO.UserDTOs
                };
         }
     }
+    public class AdminDTO
+    {
+        public string UserName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Gender { get; set; }
+        public string FullName { get; set; }
+    }
 }
