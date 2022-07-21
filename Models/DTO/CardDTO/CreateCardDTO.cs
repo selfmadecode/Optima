9f5c014182fd8550a6fd56955409ea1425e7c9aa
Oke Paul <paulayomikun@gmail.com>
2022-07-21 21:39:05 +0100

    UPDATED CARD SERVICE

--- Models/DTO/CardDTO/CreateCardDTO.cs
+++ Models/DTO/CardDTO/CreateCardDTO.cs
@@ -52,4 +52,36 @@ namespace Optima.Models.DTO.CardDTO
             return new BaseResponse<bool>();
         }
     }
+
+    public class UpdateCardImageDTO
+    {
+        public IFormFile Logo { get; set; }
+
+        public async Task<BaseResponse<bool>> Validate(IConfiguration configuration)
+        {
+
+            var fileSize = configuration.GetValue<int>("FileSize");
+
+            if (!(Logo is null))
+            {
+                if (Logo.Length > fileSize * fileSize)
+                {
+                    return new BaseResponse<bool>("Logo file size must not exceed 1Mb", new List<string> { "Logo file size must not exceed 1Mb" });
+                }
+
+
+                var allowedExt = new string[] { "jpg", "png", "jpeg" };
+                var ext = Path.GetExtension(Logo.FileName).ToLower();
+
+                var extensionValid = allowedExt.ToList().Any(x => $".{x}".Equals(ext, StringComparison.InvariantCultureIgnoreCase));
+
+                if (!extensionValid)
+                {
+                    return new BaseResponse<bool>("Logo file type must be .jpg or .png or .jpeg", new List<string> { "Logo file type must be .jpg or .png or .jpeg" });
+                }
+            }
+
+            return new BaseResponse<bool>();
+        }
+    }
 }
